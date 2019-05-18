c5e4296443e966f4bc1d2e839620451457c91989
ejones2 <47126947+ejones2@users.noreply.github.com>
2019-05-18 01:58:50 -0400

    index.routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const userRoutes = require("./users");
+const postRoutes = require("./posts");
 
 const constructorMethod = app => {
   app.use("/users", userRoutes);
+  app.use("/posts", postRoutes);
 
   app.use("*", (req, res) => {
     res.sendStatus(404);
