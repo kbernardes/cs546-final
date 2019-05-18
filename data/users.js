80979a46c998f9d5b962e04acad46473f0e6df22
kyle <kyle.bernardes@gmail.com>
2019-05-18 06:58:13 -0400

    f

--- data/users.js
+++ data/users.js
@@ -5,6 +5,25 @@ const users = mongoCollections.users;
 const uuid = require("node-uuid");
 const threads = require("./threads");
 
+let forums = [
+    {
+        title: "Mafia Game Signups",
+        threads: []    
+    },
+    {
+        title: "Ongoing Games",
+        threads: []
+    },
+    {
+        title: "Game Discussion",
+        threads: []
+    },
+    {
+        title: "General Discussion",
+        threads: []
+    }
+]
+
 async function createUser(username, password, email, firstName, lastName)
 {
     if (!username || typeof username != "string")
