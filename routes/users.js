cfd3c94f0fd4e46af0d9237dc09d11865e39b9e7
josephkuhn <jkuhn@stevens.edu>
2019-05-18 06:21:36 -0400

    k

--- routes/users.js
+++ routes/users.js
@@ -64,7 +64,7 @@ router.post("/signup", async (req, res) => {
         const user = await data.users.createUser(username, password, email, firstName, lastName);
         req.session.sessionID = uuid.v4();
         user.sessionID = req.session.sessionID;
-        res.redirect("/users" );
+        res.redirect("/users");
     }
     else{
         res.status(402).render("error", {information: "Username already taken, please try a different name"});
@@ -73,17 +73,96 @@ router.post("/signup", async (req, res) => {
 
 router.get("/logout", async(req, res) => {
     let user = await data.users.userSID(req.session.sessionID);
-    req.session.sessionID = undefined;
-    user.sessionID = req.session.sessionID;
+    //req.session.sessionID = undefined;
+    user.sessionID = undefined;
+    req.session.regenerate(function (err) 
+    {
+        req.session.sessionID = "";
+    });
     res.render("logout");
 });
 
 router.get("/profile/:username", async (req, res) => {
+    if (username === req.session.username)
+    {
+        res.redirect("/infochange");
+    }
+    else
+    {
+        res.redirect("/profile");
+    }
     // renders different file if user is accessing their own 
-    res.redirect("/users");
+    //res.redirect("/users");
 });
 
 router.put("/profile/:username", async (req, res) => {
+    const userInfo = req.body;
+    let theUser = await data.users.findUser(username);
+
+  if (!userInfo) {
+    res.status(400).json({ error: "You must provide data to update a user's info." });
+    return;
+  }
+
+  if (userInfo.username)
+  {
+    if (userInfo.username !== "")
+    {
+        await changeUsername({_id: theUser._id}, userInfo.username);
+    }
+    else
+    {
+        
+    }
+  }
+
+  if (!postInfo.newTitle && !postInfo.newContent) {
+    res.status(400).json({ error: "You must provide a title and/or content for the post" });
+    return;
+  }
+
+  try {
+    await postData.getPostById(req.params.id);
+  } catch (e) {
+    res.status(404).json({ error: "Post not found" });
+    return;
+  }
+
+  if (!postInfo.newTitle)
+  {
+    try {
+        const updatedPost = await postData.updateContent(req.params.id, postInfo.newContent);
+        res.json(updatedPost);
+      } catch (e) {
+        res.sendStatus(500);
+      }
+  }
+
+  else if (!postInfo.newContent)
+  {
+    try {
+        const updatedPost = await postData.updateTitle(req.params.id, postInfo.newTitle);
+        res.json(updatedPost);
+      } catch (e) {
+        res.sendStatus(500);
+      }
+  }
+
+  else
+  {
+    try {
+        await postData.updateTitle(req.params.id, postInfo.newTitle);
+      } catch (e) {
+        res.sendStatus(500);
+      }
+
+    try {
+        const updatedPost = await postData.updateContent(req.params.id, postInfo.newContent);
+        res.json(updatedPost);
+        } catch (e) {
+        res.sendStatus(500);
+    }
+  }
     // user must be logged in and can only update their own profile
 });
 
