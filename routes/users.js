cff9516eb599dbaa3dfd536ea0f0e7c11752fc90
kyle <kyle.bernardes@gmail.com>
2019-05-19 18:00:37 -0400

    good

--- routes/users.js
+++ routes/users.js
@@ -113,69 +113,97 @@ router.get("/logout", async(req, res) => {
     res.render("logout");
 });
 
-/*router.get("/profile/:username", async (req, res) => {
-    let user = await users.findUser(username);
-    res.render("profile", {data: user});
-    // renders different file if user is accessing their own 
-    //res.redirect("/users");
-});*/
-
-router.put("/profile/:username", async (req, res) => {
+router.get("/profile/:username", async (req, res) => {
     try {
-        let user = await data.users.userSID(req.session.sessionID);
-        res.render("infochange", {data: user});
-        const userInfo = req.body;
-        //let theUser = await data.users.findUser(username);
+        const loggedInUser = await usersData.userSID(req.session.sessionID);
+        const user = await usersData.findUser(req.params.username);
+        let myAccount = false;
 
-        if (!userInfo) {
-            res.status(400).json({ error: "You must provide data to update a user's info." });
-            return;
+        if (loggedInUser === user) {
+            myAccount = true;
         }
 
+        res.render("profile", {user: user})
+
+        // let user = await data.users.userSID(req.session.sessionID);
+        // res.render("infochange", {data: user});
+        // const userInfo = req.body;
+        // //let theUser = await data.users.findUser(username);
+
+        // if (!userInfo) {
+        //     res.status(400).json({ error: "You must provide data to update a user's info." });
+        //     return;
+        // }
+
     
-        if(userInfo.username != null)
-            await data.users.changeUsername({_id: theUser._id}, userInfo.username);  
-    
+        // if(userInfo.username != null)
+        //     await data.users.changeUsername({_id: theUser._id}, userInfo.username); 
     } catch (e) {
         res.status(400).json({ error: "That name that is already taken. Please try something else." });
         return;
     }
+});
 
-  /*try {
-    await postData.getPostById(req.params.id);
-  } catch (e) {
-    res.status(404).json({ error: "Post not found" });
-    return;
-  }*/
+router.put("/profile/:username", async (req, res) => {
+    // try {
+    //     const loggedInUser = await usersData.userSID(req.session.sessionID);
+    //     const user = await usersData.findUser(req.params.username);
+    //     let myAccount = false;
 
-  try {
-    if(userInfo.password != null)
-        await data.users.changePassword({_id: theUser._id}, userInfo.password);
-} catch (e) {
-    res.status(400).json({ error: "You entered an invalid password. Please try something else." });
-    return;
-}
+    //     if (loggedInUser === user) {
+    //         myAccount = true;
+    //     }
 
-try {
-    if(userInfo.firstName != null)
-        await data.users.changeFirstName({_id: theUser._id}, userInfo.firstName);
-} catch (e) {
-    res.status(400).json({ error: "You entered an invalid first name. Please try something else." });
-    return;
-}
+    //     res.render("profile", {user: user})
 
-try {
-    if(userInfo.lastName != null)
-        await data.users.changeLastName({_id: theUser._id}, userInfo.lastName);
-} catch (e) {
-    res.status(400).json({ error: "You entered an invalid last name. Please try something else." });
-    return;
-}
-    }
-    catch(e){
-        res.status(400).json({ error: "Could not find user by Session ID" }); 
+        // let user = await data.users.userSID(req.session.sessionID);
+        // res.render("infochange", {data: user});
+        // const userInfo = req.body;
+        // //let theUser = await data.users.findUser(username);
+
+        // if (!userInfo) {
+        //     res.status(400).json({ error: "You must provide data to update a user's info." });
+        //     return;
+        // }
+
+    
+        // if(userInfo.username != null)
+        //     await data.users.changeUsername({_id: theUser._id}, userInfo.username); 
+    // } catch (e) {
+    //     res.status(400).json({ error: "That name that is already taken. Please try something else." });
+    //     return;
+    // }
+
+    /*try {
+        await postData.getPostById(req.params.id);
+    } catch (e) {
+        res.status(404).json({ error: "Post not found" });
         return;
-    }
+    }*/
+
+    // try {
+    //     if(userInfo.password != null)
+    //         await data.users.changePassword({_id: theUser._id}, userInfo.password);
+    // } catch (e) {
+    //     res.status(400).json({ error: "You entered an invalid password. Please try something else." });
+    //     return;
+    // }
+
+    // try {
+    //     if(userInfo.firstName != null)
+    //         await data.users.changeFirstName({_id: theUser._id}, userInfo.firstName);
+    // } catch (e) {
+    //     res.status(400).json({ error: "You entered an invalid first name. Please try something else." });
+    //     return;
+    // }
+
+    // try {
+    //     if(userInfo.lastName != null)
+    //         await data.users.changeLastName({_id: theUser._id}, userInfo.lastName);
+    // } catch (e) {
+    //     res.status(400).json({ error: "You entered an invalid last name. Please try something else." });
+    //     return;
+    // }
 });
 
 router.get("/all", async (req,res) => {
